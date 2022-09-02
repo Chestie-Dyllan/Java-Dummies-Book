c8d47b7cc3fa3b23aba3ed5c63e5f8a1fdb01800
Dyllan Van Den Heever <98019185+Chestie-Dyllan@users.noreply.github.com>
2022-09-02 12:12:38 +0200

    Upload Chap4

---
+++ Book4/src/Chap4/Summary.java
@@ -0,0 +1,43 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Chap4;
+
+/**
+ *
+ * @author dylla
+ */
+public class Summary {
+    /*
+    LinkedList Class
+    
+    - A linked list is a collection in which every object in the list maintains with it a
+      pointer to the following object in the list and another pointer to the preceding
+      object in the list
+    
+    - Linked lists require more memory than arrays and are slower
+      than arrays when it comes to simple sequential access.
+    
+    Adding Items to a LinkedList
+    
+    - The LinkedList class gives you many ways to add items to the list. The most basic
+      is the add method, which works pretty much the same way that it does for the
+      ArrayList class.
+    
+     Example:
+      LinkedList<String> officers = new LinkedList<String>();
+      officers.add("Blake");
+      officers.add("Burns");
+      officers.add("Houlihan");
+      officers.add("Pierce");
+      officers.add("McIntyre");
+     for (String s : officers)
+     System.out.println(s);
+     
+    Retrieving Items from a LinkedList
+    
+    - As with the ArrayList class, you can use the get method to retrieve an item based
+      on its index.
+*/
+}
