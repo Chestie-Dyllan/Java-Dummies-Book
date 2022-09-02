fee3abdb5997c5454f0acb944492f9e46a8fd013
Dyllan Van Den Heever <98019185+Chestie-Dyllan@users.noreply.github.com>
2022-09-02 12:41:42 +0200

    Update Chap1

---
+++ Book4/src/Chap1/Summary.java
@@ -0,0 +1,28 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Chap1;
+
+/**
+ *
+ * @author dylla
+ */
+public class Summary {
+    /*
+    Working with Strings
+    
+    - Strings are reference types, not value types, such as int or boolean. As a
+      result, a string variable holds a reference to an object created from the
+      String class, not the value of the string itself.
+    
+     Exmpl: String line1 = "Oh what a beautiful morning!";
+    
+    - Strings can include escape sequences.
+    
+    Using the String Class
+    
+    - The String class is the class you use to create string objects.
+    - A String object is immutable, which means that it can’t be changed.
+    */
+}
