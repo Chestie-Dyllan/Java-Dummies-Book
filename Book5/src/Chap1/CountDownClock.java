ed2c68ba9fb158474ac3621247ee850707e7c82a
Dyllan Van Den Heever <98019185+Chestie-Dyllan@users.noreply.github.com>
2022-09-04 14:51:53 +0200

    Upload Book 5 and chapter 1

---
+++ Book5/src/Chap1/CountDownClock.java
@@ -0,0 +1,27 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Chap1;
+
+/**
+ *
+ * @author dylla
+ */
+public class CountDownClock extends Thread {
+
+    public void run() {
+        for (int t = 20; t >= 0; t--) {
+
+        }
+
+        {
+            System.out.println("T minus " + t);
+            try {
+                Thread.sleep(1000);
+
+            } catch (InterruptedException e) {
+            }
+        }
+    }
+}
